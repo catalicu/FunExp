96a1566ec7e30ad8f900d59ef5b93ef8840820a1
catalicu <catalicu@users.noreply.github.com>
2024-02-22 09:57:04 -0800

    found the fig6 code - still needs editing

--- Fig6_FunASV_plots.R
+++ Fig6_FunASV_plots.R
@@ -29,16 +29,71 @@ taxatable=read.table('input_data/taxtable_FunExp12023-01-04.txt', header=TRUE)
 ASVslope_sorted=read.table('output_tables/ASVslope_sorted52024-02-21.txt', header=TRUE)
 
 # list of ASV codes to go through:
-ASVfun_names=names(ASVtable.fundiv1)[1:1375]
+#ASVfun_names=names(ASVtable.fundiv1)[1:1375]
+
+# from v10 (unedited:
+### Load datasets: 
+#Import the tables and code from FunASV_v4_taxFunct2:
+ # * ASVtable.fundiv contains ASV(code) relative abundances, functional data and diversity data for each sample
+#* taxtable contains taxonomic affiliation per ASVcode
+#Functional data is not necessary here explicitly, but it means we are using the same processed tables to rank and compare taxa. 
+
+# start from the sorted table of slopes
+ASVslope_sorted=read.table('output_files/ASVslope_sorted_FunExp12023-01-07.txt', header=TRUE)
+
+
+### Calculate: mean abundance, occurrence and mean functional output per ASV
+
+#presence absence
+ASVpa=ASVtable
+ASVpa[(ASVpa!=0)]=1
+ASVpa.freq=colSums(ASVpa)/(dim(ASVpa)[1])
+#relative abundance
+ASVrel.abund=colSums(ASVtable)
+
+### Combine these values into useable tables
+
+#### Relative abundance vs occupancy.
+#To better visualize this relationship:
+ # * create a data frame with the relative abundance and the occupancy data
+#* classify the taxa into 3 groups based on their occurrence and abundance
+#* save this table for later use
+# data frame with abundance and freq
+ASVrel.abund.pa=data.frame(ASVrel.abund, ASVpa.freq)
+# highlight frequency taxa
+ASVrel.abund.pa$Frequency=ASVrel.abund.pa$ASVpa.freq
+ASVrel.abund.pa[which(ASVrel.abund.pa$ASVpa.freq>0.035),3]='Frequent'
+ASVrel.abund.pa[which(ASVrel.abund.pa$ASVpa.freq<0.035),3]='Unfrequent'
+ASVrel.abund.pa[which(ASVrel.abund.pa$ASVrel.abund>1000000),3]='Most Frequent'
+ASVrel.abund.pa$Frequency=factor(ASVrel.abund.pa$Frequency, levels = c('Unfrequent', 'Frequent', 'Most Frequent'))
+
+# Save this table:
+ASVrel.abund.pa_printPATH = paste(outPATH, 'FunExp_Relabund_freqRanking_perASV', date,'.txt', sep='')
+write.table(ASVrel.abund.pa, file=ASVrel.abund.pa_printPATH, sep='\t')
+
+#### Add the Functional Ratio to the abundance and occupancy
+
+ASVrel.abund.pa.fun=data.frame(ASVrel.abund.pa) #, ASVfunRat) removed funRat because it was a weird metric
+ASVrel.abund.pa.fun$ASVcode=rownames(ASVrel.abund.pa.fun)
+#save this table: major output
+ASVrel.pa.fun_table = paste(outPATH, 'ASV_relabun_pa_fun_table_FunExp1', date,'.txt', sep='')
+write.table(ASVrel.abund.pa.fun, ASVrel.pa.fun_table, sep="\t")
+#Add the information on taxon classification according to their relationship with function (from FunASV_v8_taxAnalysis_abund-funct_B.R)
+# Join relative abundance, occurrence and slope classification
+ASVrel.abund.pa.fun.sorted=left_join(ASVrel.abund.pa.fun, ASVslope_sorted)
+# label the taxa that were not analyzed for slope as 'undetermined' slopes.
+ASVrel.abund.pa.fun.sorted[which(is.na(ASVrel.abund.pa.fun.sorted$Slope)),6]='Undetermined'
+
+ggplot(ASVrel.abund.pa.fun.sorted, aes(log(ASVrel.abund), ASVpa.freq)) + geom_point(aes(fill=Slope), size=3, shape=21) + Theme +ggtitle('Relationship between ASV occupancy (frequency) and relative abundance') + scale_fill_manual(values=c('black', 'grey', 'white', 'blue')) + ylab('ASV occurrence') + xlab('ASV Relative Abundance')
 
 # Before melting, reduce the dataset to taxa that we were able to relate to function (in scritp v8)
 # select the columns that will go into the long format:
 # include treatment, leaf age, and taxa that were realted to function
 ASVfor_melt=ASVtable.fundiv[,c(which(colnames(ASVtable.fundiv)%in%(ASVslope_sorted$ASVcode)),1377:1394)]
 # Then use melt to go from the wide table format to the long table format. 
-otumelt_fun.div=melt(ASVfor_melt, id=c("richness"))
+otumelt_fun.div=melt(ASVfor_melt, id=c("leaf_age_weeks2"))
 length(names(otumelt_fun.div))
-names(otumelt_fun.div)=c('richness',"ASVcode", "Abundance") # the last two columns will represent the ASV code an the abundance value 
+names(otumelt_fun.div)=c('leaf_age_weeks2',"ASVcode", "Abundance") # the last two columns will represent the ASV code an the abundance value 
 head(otumelt_fun.div)
 
 #Clean the tax table to add tax information to the long version of the ASV table with metadata (otumelt)
@@ -53,6 +108,7 @@ otumelt2.div$Abundance=as.numeric(as.character(otumelt2.div$Abundance))
 #Also create a table with abundance per family (object: meanlist.fam3).
 
 ASVlist.div=unique(otumelt2.div$ASVcode)	#866 taxa
+
 # calculate means for abundance per taxon
 ASVlist_means.div=ddply(otumelt2.div, .(ASVcode), summarize,  Abund=mean(Abundance, na.rm=TRUE))
 ASVlist_means.div=ASVlist_means.div[order(ASVlist_means.div$Abund, decreasing=TRUE),]
