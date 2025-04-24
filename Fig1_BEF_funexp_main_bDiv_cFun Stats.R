da9379bcb03930d1bf8c70f1ddeed6f4e993ccaf
catalicu <catalicu@users.noreply.github.com>
2025-04-24 15:38:52 -0700

    fig1 plot and stats updated, added file with session info

---
+++ Fig1_BEF_funexp_main_bDiv_cFun Stats.R
@@ -0,0 +1,143 @@
+#title: "Fig1_BEF_funexp_main_bDiv_cFun.R"
+#author: "Dr CG"
+#date:"3/28/2025"
+
+# Description:
+# This script performs statistical testing to support patterns portrayed in 
+# figure 1, including a) the BEF relationship, b) change in diversity with leaf
+# age and c) change in function with leaf age. 
+
+#########################
+## Stats ##
+#########################
+
+# Load tables
+ASVtable=read.table('input_data/ASVtable_FunExp12021-03-24.txt', header=TRUE)
+TAXtable=read.table('input_data/TAXtable_FunExp12021-03-24.txt', header=TRUE)
+meta.table=read.table('input_data/METAtable_FunExp12021-03-24_sampleNamesFIX.txt', header=TRUE)
+
+div.table=read.table('input_data/MetaDiv_table_FunExp12021-03-30.txt', header=TRUE)
+colnames(div.table)[2]='Treatment'
+
+fun.table=read.table('input_data/FunExp_metadata.txt', header=TRUE)
+
+# Merge diversity and functional data and make sure weeks are numeric
+fundiv.table=left_join(fun.table, div.table)
+fundiv.table$leaf_age_weeks2=as.numeric(fundiv.table$leaf_age_weeks)
+
+# remove rows with no diversity data for BEF analyses
+fundiv.table_BEF1=fundiv.table[-which(is.na(fundiv.table$SampleID)),]
+fundiv.table_BEF2=fundiv.table_BEF1[-which(fundiv.table_BEF1$W_Change==min(fundiv.table_BEF1$W_Change)),]
+
+# is the highest diversity point an outlier?
+boxplot(fundiv.table_BEF1$richness, main = "Boxplot of MPG", col = "lightblue")
+  library(outliers)
+  grubbs.test(fundiv.table_BEF1$richness)
+fundiv.table_BEF=fundiv.table_BEF2[-which(fundiv.table_BEF2$richness==(max(fundiv.table_BEF2$richness))),]
+
+#### GLM div ~ richness * leaf age * protozoan treatment
+# The stats can be assessed as a linear model on leaf age and protozoan 
+# treatment as a factor.
+mod.rich.null=lm(W_Change~1, data=fundiv.table_BEF2)
+mod.rich.null.gamma=glm(W_Change~1, data=fundiv.table_BEF2, family=Gamma(link='log'))
+mod.rich=lm(W_Change~log(richness+1)*leaf_age_weeks*treatment, data=fundiv.table_BEF)
+mod.rich2=lm(W_Change~(richness)*leaf_age_weeks*treatment, data=fundiv.table_BEF)
+mod.rich3=lm(W_Change~(richness)*treatment, data=fundiv.table_BEF)
+mod.rich3b=lm(W_Change~log(richness)*treatment, data=fundiv.table_BEF)
+mod.rich4=lm(W_Change~(richness)*leaf_age_weeks, data=fundiv.table_BEF)
+mod.rich4b=lm(W_Change~log(richness)*leaf_age_weeks, data=fundiv.table_BEF)
+mod.rich5=lm(W_Change~(richness), data=fundiv.table_BEF)
+mod.rich6=lm(W_Change~log(richness), data=fundiv.table_BEF)
+mod.rich7=glm(W_Change~(richness)*(leaf_age_weeks), data=fundiv.table_BEF2, family=Gamma(link='log'))
+mod.rich8=glm(W_Change~log(richness)*(leaf_age_weeks)*treatment, data=fundiv.table_BEF, family=Gamma(link='log'))
+mod.rich9=glm(W_Change~log(richness)*(leaf_age_weeks)+treatment, data=fundiv.table_BEF, family=Gamma(link='log'))
+
+#Test the models:
+# lowest AIC
+AIC(mod.rich.null, mod.rich, mod.rich2, mod.rich3,  mod.rich3b, mod.rich4,mod.rich4b, mod.rich5, mod.rich6, mod.rich7, mod.rich8)
+# test the best modelmod.rich7# test the best model against the null
+Anova(mod.rich7)
+
+#### GLM div ~ evenness * leaf age * protozoan treatment
+# same model for evenness - for sup mat
+mod.eve.null=lm(W_Change~1, data=fundiv.table_BEF)
+mod.eve=lm(W_Change~log(evenness+1)*leaf_age_weeks*treatment, data=fundiv.table_BEF)
+mod.eve2=lm(W_Change~(evenness)*leaf_age_weeks*treatment, data=fundiv.table_BEF)
+mod.eve3=lm(W_Change~(evenness)*treatment, data=fundiv.table_BEF)
+mod.eve3b=lm(W_Change~log(evenness+1)*treatment, data=fundiv.table_BEF)
+mod.eve4=lm(W_Change~(evenness)*leaf_age_weeks, data=fundiv.table_BEF)
+mod.eve4b=lm(W_Change~log(evenness+1)*leaf_age_weeks, data=fundiv.table_BEF)
+mod.eve5=lm(W_Change~(evenness), data=fundiv.table_BEF)
+mod.eve6=lm(W_Change~log(evenness), data=fundiv.table_BEF)
+
+#Test the models:
+# lowest AIC
+AIC(mod.eve.null, mod.eve, mod.eve2, mod.eve3,  mod.eve3b, mod.eve4,mod.eve4b, mod.eve5, mod.eve6)
+# the null model had lowest AIC
+
+#########################
+## Change in a) diversity and b) function with leaf age ##
+#########################
+
+## DIVERSITY
+## edit tables
+# create leaf age column
+meta.table$leaf_age_weeks=as.numeric(substr(meta.table$leaf_age, 2,3))
+
+## Calculate diversity metrics
+richness=specnumber(ASVtable)
+shannon=diversity(ASVtable, index='shannon')
+simpson=diversity(ASVtable, index='simpson')
+evenness=shannon/log(richness)
+
+# create data frame
+meta.div=data.frame(meta.table, richness, shannon, simpson, evenness)
+
+# remove controls
+meta.div2=meta.div[-c(which(meta.div$treatment=='F50'), 
+                      which(meta.div$treatment=='water_control'), 
+                      which(meta.div$treatment=='broth_control'), 
+                      which(meta.div$treatment=='TAI_control'), 
+                      which(meta.div$treatment=='TAO_control')),]
+data.frame(dim(meta.div), dim(meta.div2)) # check that the column was eliminated by looking at the dimensions of the datasets
+
+# Statistical models for diversity
+mod.rich.null=lm(log(richness)~1, data=meta.div2)
+mod.rich=lm(log(richness)~leaf_age_weeks, data=meta.div2)
+mod.rich1a=lm(log(richness)~treatment, data=meta.div2)
+mod.rich2=lm(log(richness)~leaf_age_weeks*treatment, data=meta.div2)
+mod.rich3=lm(log(richness)~leaf_age_weeks+treatment, data=meta.div2)
+mod.rich4=glm(log(richness)~treatment, data=meta.div2, family=Gamma(link='log'))
+
+#Test the models:
+AIC(mod.rich.null, mod.rich, mod.rich1a,  mod.rich2, mod.rich3, mod.rich4)
+Anova(mod.rich)
+
+## FUNCTION
+# load data
+div.table=read.table('input_data/MetaDiv_table_FunExp12021-03-30.txt', header=TRUE)
+colnames(div.table)[2]='Treatment'
+# function diversity
+fun.table=read.table('input_data/FunExp_metadata.txt', header=TRUE)
+
+# edit tables
+fundiv.table=left_join(fun.table, div.table)
+fundiv.table$leaf_age_weeks2=as.numeric(fundiv.table$leaf_age_weeks)
+fundiv.table=fundiv.table[-which(fundiv.table$W_Change==min(fundiv.table$W_Change)),]
+fundiv.table2=fundiv.table[-which(is.na(fundiv.table$SampleID)),]
+
+## Statistical models for function
+mod2.age.null=lm(W_Change~1, data=fundiv.table2)
+mod2.age1=lm(W_Change~leaf_age_weeks, data=fundiv.table2)
+mod2.age2=lm(W_Change~treatment, data=fundiv.table2)
+mod2.age3=lm(W_Change~leaf_age_weeks*treatment, data=fundiv.table2)
+mod2.age4=lm(W_Change~leaf_age_weeks+treatment, data=fundiv.table2)
+mod2.age5=glm(W_Change~leaf_age_weeks, data=fundiv.table2, family=Gamma(link='log'))
+mod2.age6=glm(W_Change~leaf_age_weeks*treatment, data=fundiv.table2, family=Gamma(link='log'))
+mod2.age7=glm(W_Change~leaf_age_weeks+treatment, data=fundiv.table2, family=Gamma(link='log'))
+
+#Test the models:
+# lowest AIC
+AIC(mod2.age.null, mod2.age1, mod2.age2, mod2.age3, mod2.age4, mod2.age5, mod2.age6, mod2.age7)
+# test the best model against the null
+Anova(mod2.age5, test.statistic = 'Wald')
---
+++ Fig1_BEF_funexp_main_bDiv_cFun.R
@@ -0,0 +1,127 @@
+#title: "Fig1_BEF_funexp_main_bDiv_cFun.R"
+#author: "Dr CG"
+#date:"3/28/2025"
+
+# Description:
+# This script creates the three panels for figure 1 including a) the BEF
+# relationship, b) the change of diversity with leaf age and c) the change
+# in function with leaf age. 
+
+# Libraries
+library(ggplot2)
+library(vegan)
+library(dplyr)
+library(lme4)
+library(nlme)
+library(gridExtra)
+library(car)
+library(effects)
+library(patchwork)
+
+#########################
+## BEF plots (a) ##
+#########################
+
+# Plot themes
+## With legend
+Theme=theme_classic(base_size=11, base_family="Helvetica") +
+  theme(axis.line = element_line(size = 1, colour = "black", linetype = "solid")) +theme(plot.title = element_text(size = 12))
+## Without legends
+Theme2=Theme+ theme(legend.position="none") + theme(panel.border=element_rect(fill=NA))
+
+ASVtable=read.table('input_data/ASVtable_FunExp12021-03-24.txt', header=TRUE)
+TAXtable=read.table('input_data/TAXtable_FunExp12021-03-24.txt', header=TRUE)
+meta.table=read.table('input_data/METAtable_FunExp12021-03-24_sampleNamesFIX.txt', header=TRUE)
+
+div.table=read.table('input_data/MetaDiv_table_FunExp12021-03-30.txt', header=TRUE)
+colnames(div.table)[2]='Treatment'
+
+fun.table=read.table('input_data/FunExp_metadata.txt', header=TRUE)
+
+# Merge diversity and functional data and make sure weeks are numeric
+fundiv.table=left_join(fun.table, div.table)
+fundiv.table$leaf_age_weeks2=as.numeric(fundiv.table$leaf_age_weeks)
+
+# remove rows with no diversity data for BEF analyses
+fundiv.table_BEF1=fundiv.table[-which(is.na(fundiv.table$SampleID)),]
+fundiv.table_BEF2=fundiv.table_BEF1[-which(fundiv.table_BEF1$W_Change==min(fundiv.table_BEF1$W_Change)),]
+
+# is the highest diversity point an outlier?
+boxplot(fundiv.table_BEF1$richness, main = "Boxplot of MPG", col = "lightblue")
+  grubbs.test(fundiv.table_BEF1$richness)
+fundiv.table_BEF=fundiv.table_BEF2[-which(fundiv.table_BEF2$richness==(max(fundiv.table_BEF2$richness))),]
+
+# model selected out of options 
+mod.rich7=glm(W_Change~log(richness)*(leaf_age_weeks), data=fundiv.table_BEF, family=Gamma(link='log'))
+effect_data=data.frame(effect('log(richness)', mod.rich7)) # predict on one variable of a model
+
+BEFmain_fig= ggplot(fundiv.table_BEF, aes(log(richness), W_Change)) + 
+  geom_jitter(color='black',shape=21, aes(size=leaf_age_weeks, fill=treatment))+ 
+  geom_line(data=effect_data, aes(x=log(richness), y=fit), 
+            linetype='dashed', linewidth=1) +
+  Theme2 + xlab('log(number of ASV)') + 
+  ylab('Weight change (g)') + 
+  annotate(geom='text', y=0.0325, x=2.7, label='a.')+
+  scale_fill_manual(values=c('black','white')) 
+
+plot(BEFmain_fig)
+#quartz.save('Figures/Fig_BEFmain.png', type='png', dpi=300)
+
+### evenness - for sup mat
+
+# model selected was not significantly different than the null model.
+mod.eve.null=lm(W_Change~1, data=fundiv.table_BEF)
+mod.eve4=lm(W_Change~(evenness)*leaf_age_weeks, data=fundiv.table_BEF)
+
+#########################
+## Age plots (b and c) ##
+#########################
+
+## format data
+meta.table$leaf_age_weeks=as.numeric(substr(meta.table$leaf_age, 2,3))
+# Remove controls from metadata
+meta.table2=meta.table[-c(which(meta.table$treatment=='F50'),
+                          which(meta.table$treatment=='water_control')),]
+# Remove controls from ASVtable 
+ASVtable2=ASVtable[-c(which(meta.table$treatment=='F50'),
+                      which(meta.table$treatment=='water_control')),]
+
+# Calculate diversity metrics
+richness=specnumber(ASVtable)
+shannon=diversity(ASVtable, index='shannon')
+simpson=diversity(ASVtable, index='simpson')
+evenness=shannon/log(richness)
+meta.div=data.frame(meta.table, richness, shannon, simpson, evenness)
+
+# remove controls
+meta.div2=meta.div[-c(which(meta.div$treatment=='F50'), 
+                      which(meta.div$treatment=='water_control'), 
+                      which(meta.div$treatment=='broth_control'), 
+                      which(meta.div$treatment=='TAI_control'), 
+                      which(meta.div$treatment=='TAO_control')),]
+# check that the column was eliminated by looking at the dimensions of the datasets
+data.frame(dim(meta.div), dim(meta.div2)) 
+# Merge function and diversity table
+fundiv.table2=left_join(fundiv.table, meta.table2)
+
+# Figures
+rich.plot=ggplot(meta.div2, aes(leaf_age_weeks, log(richness))) + 
+  geom_jitter(size=3, color='black', shape=21, aes( fill=treatment))+  
+  geom_smooth(method='lm', se=FALSE, color='black', linetype='dashed') + 
+  ylab('log(ASV richness)') + xlab('Leaf age (weeks)')  + 
+  Theme2 + scale_fill_manual(values=c('black', 'white')) + 
+  theme(legend.position = 'none') +
+  annotate('text', x=0, y=5.5, label='b.')
+
+fun.plot=ggplot(fundiv.table2, aes(leaf_age_weeks, W_Change)) + 
+  geom_jitter(size=3, color='black', shape=21, aes(fill=treatment)) + 
+  Theme2 +  scale_fill_manual(values=c('black', 'white')) + 
+  ylab('Weight change (g)') + xlab('Leaf age (weeks)')  + 
+  geom_smooth(method='gam', se=FALSE, color='black', linetype='dashed') +
+  annotate('text', x=0, y=0.035, label='c.')
+
+## Merge all plots together and save
+quartz(width=6, height=5)
+layout2 = (BEFmain_fig | plot_spacer()) / (rich.plot | fun.plot) 
+layout2
+quartz.save('Figures/Fig_BEF_time_ADiv_BFun.png', type='png', dpi=300)
